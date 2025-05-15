e0668405fd2da513060e9f19fc5b458f2651fc43
yash thakkar <itsyash1401@gmail.com>
2025-05-15 12:20:41 +0530

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import Nav from "./components/Nav";
+import Jumbotron from "./components/Jumbotron";
+import SoundSection from "./components/SoundSection";
+import DisplaySection from "./components/DisplaySection";
+import WebgiViewer from "./components/WebgiViewer";
 
-function App() {
-
+const App = () => {
   return (
     <div className="App">
-      Hello
+      <Nav />
+      <Jumbotron />
+      <SoundSection />
+      <DisplaySection />
+      <WebgiViewer />
     </div>
   );
-}
+};
 
 export default App;
